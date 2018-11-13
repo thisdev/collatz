d5229c82a528fefae2324879717b7611ab1625ba
thisdev <thisdev@bitlager.de>
2018-11-13 23:27:10 +0100

    collatz functionality implemented

---
+++ collatz.js
@@ -0,0 +1,14 @@
+function collatz(n) {
+    if (isNaN(n) || n <= 0) return;
+    for (let i = 0; i <= 100; i++) {
+        if (n % 2 === 0) {
+            n = n / 2;
+        }
+        else {
+            n = n * 3 + 1;
+        }
+        console.log('Numbers: ' + n);
+    }
+}
+// natural numbers > 0 allowed
+collatz(443);
